d6920f16e47b4fa2bc1aedd41b081ee997944c46
Jae <jaeladu1@gmail.com>
2022-06-06 21:26:58 -0300

    added missing files

---
+++ components/service.js
@@ -0,0 +1,17 @@
+function mountMinimailService(service, containerEl) {
+   const title = service.fields.title;
+   const description = service.fields.description;
+
+   const serviceEl = document.createElement("div");
+   serviceEl.classList.add("service");
+   serviceEl.innerHTML = `
+<img
+   src="../elements/desktop.svg"
+   alt="service logo"
+   class="service__logo"
+/>
+<h3 class="service__title">${title}</h3>
+<p class="service__description">${description}</p>`;
+
+   containerEl.append(serviceEl);
+}
